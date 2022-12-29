60d4ae327c8e408948c6bba0037a2d033cc373ad
Dmitrijs Trizna <dtrizna@microsoft.com>
2022-12-29 14:51:57 +0100

    working PE classifier example

--- work/speakeasy_4_PEClassifier/classifier_test.py
+++ work/speakeasy_4_PEClassifier/classifier_test.py
@@ -4,12 +4,16 @@ from nebula import PEHybridClassifier
 
 speakeasyConfigFile = r"C:\Users\dtrizna\Code\nebula\emulation\_speakeasyConfig.json"
 vocabFile = r"C:\Users\dtrizna\Code\nebula\data\data_filtered\speakeasy_trainset_WithAPIargs\speakeasy_VocabSize_10000.pkl"
+outEmulationReportFolder = r"C:\Users\dtrizna\Code\nebula\tests\speakeasy_4_PEClassifier\tempSpeakEasyReports"
 
 model = PEHybridClassifier(
     vocabFile=vocabFile,
     speakeasyConfig=speakeasyConfigFile,
-    outputFolder="./tempSpeakEasyReports"
+    outputFolder=outEmulationReportFolder,
 )
 testFile = r"C:\windows\syswow64\xcopy.exe"
 static, dynamic = model.preprocess(testFile)
 
+logits = model(static, dynamic)
+print(logits)
+import pdb;pdb.set_trace()
