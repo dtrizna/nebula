b65125d581d8e984f995a68983d9a40005998745
Dmitrijs Trizna <dtrizna@microsoft.com>
2023-01-06 20:32:27 +0100

    attention models: reformer and transformer positional encoding fix

---
+++ work/speakeasy_2_Modeling/_speakeasyAttentionDimension.py
@@ -0,0 +1,26 @@
+import torch
+
+import sys
+sys.path.extend(['.', '..\\..'])
+from nebula.attention import TransformerEncoderModel, MyReformerLM
+
+model = TransformerEncoderModel(
+    vocabSize=10000,
+    maxLen=2048,
+    hiddenNeurons=[1024, 64]
+).cuda()
+
+x = torch.randint(0, 10000, (1, 2048)).cuda()
+y = model(x).detach().cpu()
+print(y.shape)
+
+model2 = MyReformerLM(
+    vocabSize=10000,
+    maxLen=2048,
+    dim=512,
+    depth=6,
+).cuda()
+
+x2 = torch.randint(0, 10000, (1, 2048)).cuda()
+y2 = model2(x2).detach().cpu()
+print(y2.shape)
