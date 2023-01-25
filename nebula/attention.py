83e3d3a0120263d2cc6a8add940af855098d6353
Dmitrijs Trizna <ditrizna@protonmail.com>
2023-01-25 07:52:23 +0100

    transformer language model

--- nebula/attention.py
+++ nebula/attention.py
@@ -10,6 +10,96 @@ from nebula.reformer import (
 )
 
 
+class TransformerEncoderLM(nn.Module):
+    def __init__(self,
+                    vocabSize: int, # size of vocabulary
+                    maxLen: int, # maximum length of input sequence
+                    dModel: int = 32, # embedding & transformer dimension
+                    nHeads: int = 8, # number of heads in nn.MultiheadAttention
+                    dHidden: int = 200, # dimension of the feedforward network model in nn.TransformerEncoder
+                    nLayers: int = 2, # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+                    numClasses: int = 1, # 1 ==> binary classification 
+                    hiddenNeurons: list = [64], # decoder's classifier FFNN complexity
+                    layerNorm: bool = False, # whether to normalize decoder's FFNN layers
+                    pretrainLayers: list = [1024], # pretrain layers
+                    dropout: float = 0.3):
+        super().__init__()
+        assert dModel % nHeads == 0, "nheads must divide evenly into d_model"
+        self.__name__ = 'Transformer'
+        self.encoder = nn.Embedding(vocabSize, dModel)
+        self.pos_encoder = PositionalEncoding(dModel, dropout)
+        encoder_layers = TransformerEncoderLayer(dModel, nHeads, dHidden, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nLayers)
+        self.d_model = dModel
+
+        self.ffnn = []
+        for i,h in enumerate(hiddenNeurons):
+            self.ffnnBlock = []
+            if i == 0:
+                self.ffnnBlock.append(nn.Linear(self.d_model * maxLen, h))
+            else:
+                self.ffnnBlock.append(nn.Linear(hiddenNeurons[i-1], h))
+
+            # add BatchNorm to every layer except last
+            if layerNorm and i < len(hiddenNeurons)-1:
+                self.ffnnBlock.append(nn.LayerNorm(h))
+
+            self.ffnnBlock.append(nn.ReLU())
+
+            if dropout:
+                self.ffnnBlock.append(nn.Dropout(dropout))
+            
+            self.ffnn.append(nn.Sequential(*self.ffnnBlock))
+        self.ffnn = nn.Sequential(*self.ffnn)
+
+        # pretrain layers
+        self.preTrainLayers = []
+        for i, h in enumerate(pretrainLayers):
+            self.preTrainBlock = []
+            if i == 0:
+                self.preTrainBlock.append(nn.Linear(hiddenNeurons[-1], h))                
+            else:
+                self.preTrainBlock.append(nn.Linear(pretrainLayers[i-1], h))
+            self.preTrainBlock.append(nn.ReLU())
+            if dropout:
+                self.preTrainBlock.append(nn.Dropout(dropout))
+            self.preTrainLayers.append(nn.Sequential(*self.preTrainBlock))
+        self.preTrainLayers.append(nn.Linear(pretrainLayers[-1], vocabSize))
+        self.preTrainLayers = nn.Sequential(*self.preTrainLayers)
+
+
+        self.fcOutput = nn.Linear(hiddenNeurons[-1], numClasses)
+
+        self.init_weights()
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        for block in self.ffnn:
+            for layer in block:
+                if isinstance(layer, nn.Linear):
+                    layer.bias.data.zero_()
+                    layer.weight.data.uniform_(-initrange, initrange)
+
+    def core(self, src: Tensor, src_mask: Optional[Tensor] = None) -> Tensor:
+        src = self.encoder(src) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        x = self.transformer_encoder(src, src_mask)
+        x = x.view(x.size(0), -1)
+        x = self.ffnn(x)
+        return x
+    
+    def pretrain(self, x: Tensor) -> Tensor:
+        x = self.core(x)
+        x = self.preTrainLayers(x)
+        return x
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.core(x)
+        out = self.fcOutput(x)
+        return out
+
+
 class TransformerEncoderModel(nn.Module):
     def __init__(self,
                     vocabSize: int, # size of vocabulary
@@ -204,23 +294,22 @@ def generate_square_subsequent_mask(sz: int) -> Tensor:
 
 
 class PositionalEncoding(nn.Module):
-    "Implement the PE function."
-
-    def __init__(self, d_model, dropout, max_len=5000):
-        super(PositionalEncoding, self).__init__()
+    """ From: https://pytorch.org/tutorials/beginner/transformer_tutorial.html """
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
         self.dropout = nn.Dropout(p=dropout)
 
-        # Compute the positional encodings once in log space.
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
-        )
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
-        self.register_buffer("pe", pe)
+        pe = torch.zeros(max_len, 1, d_model)
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
 
-    def forward(self, x):
-        x = x + self.pe[:, : x.size(1)].requires_grad_(False)
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
         return self.dropout(x)
