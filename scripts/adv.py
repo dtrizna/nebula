70103ce315beb2a0242712bbecc9c03baf43ac93
zangobot <luca.demetrio@unige.it>
2023-05-19 17:23:31 +0200

    Adding shap analysis before adversarial + adding some goodware / malware repo from quo.vadis repository (to test SHAP)

---
+++ scripts/adv.py
@@ -0,0 +1,147 @@
+import copy
+import json
+import math
+import os
+import pathlib
+import sys
+from typing import Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+import shap
+import torch
+import torch.nn.functional as F
+from torch import Tensor, sigmoid
+
+from nebula import PEDynamicFeatureExtractor
+from nebula.models.attention import TransformerEncoderChunksOptionalEmbedding
+from nebula.preprocessing import JSONTokenizerNaive
+
+# logging.basicConfig(level=logging.INFO)
+
+# correct path to repository root
+if os.path.basename(os.getcwd()) == "scripts":
+    REPOSITORY_ROOT = os.path.join(os.getcwd(), "..")
+else:
+    REPOSITORY_ROOT = os.getcwd()
+sys.path.append(REPOSITORY_ROOT)
+
+from nebula.misc import fix_random_seed
+
+fix_random_seed(0)
+
+
+def compute_score(llm, x):
+    logit = llm(x)
+    prob = sigmoid(logit)
+    print(f"\n[!!!] Probability of being malicious: {prob.item():.3f} | Logit: {logit.item():.3f}")
+
+
+def load_tokenizer():
+    with open(os.path.join(REPOSITORY_ROOT, "nebula", "objects",
+                           "speakeasy_whitespace_50000_vocab.json")) as f:
+        vocab = json.load(f)
+
+    tokenizer = JSONTokenizerNaive(
+        vocab_size=len(vocab),
+        seq_len=512,
+        vocab=vocab
+    )
+    return tokenizer
+
+
+def tokenize_sample(report_path):
+    extractor = PEDynamicFeatureExtractor()
+    filtered_report = extractor.filter_and_normalize_report(report_path)
+    tokenizer = load_tokenizer()
+    tokenized_report = tokenizer.tokenize(filtered_report)
+    encoded_report = tokenizer.encode(tokenized_report, pad=True)
+    x = Tensor(encoded_report).long()
+    return x
+
+
+def embed(llm_model, report, src_mask: Optional[Tensor] = None):
+    src = tokenize_sample(report)
+    chunks = []
+    for chunk in torch.split(src, split_size_or_sections=llm_model.chunk_size, dim=1):
+        if chunk.shape[1] < llm_model.chunk_size:
+            pad_mask = (0, llm_model.chunk_size - chunk.shape[1])
+            chunk = F.pad(chunk, pad=pad_mask)
+        chunk = llm_model.encoder(chunk) * math.sqrt(llm_model.d_model)
+        chunk = llm_model.pos_encoder(chunk)
+        chunk = llm_model.transformer_encoder(chunk, src_mask)
+        # at this stage each chunk is: (batch_size, chunk_size, d_model)
+        chunks.append(chunk)
+    # after cat it'd be: (batch_size, chunk_size * nr_of_chunks * d_model, d_model)
+    # where nr_of_chunks = int(maxLen/self.chunk_size) + 1
+    x = torch.cat(chunks, dim=1)
+    return x
+
+
+def plot_shap_values(shap_values: np.ndarray, name: str):
+    shap_values = shap_values.mean(axis=2)
+    pos_idx = shap_values[0] >= 0
+    neg_index = shap_values[0] < 0
+    pos_shap = copy.deepcopy(shap_values)[0]
+    pos_shap[neg_index] = 0
+    neg_shap = copy.deepcopy(shap_values)[0]
+    neg_shap[pos_idx] = 0
+    x = range(512)
+    plt.bar(x, pos_shap)
+    plt.bar(x, neg_shap)
+    plt.title(name)
+    plt.show()
+
+
+def load_model(skip_embedding=False):
+    pretrained_model = os.path.join(REPOSITORY_ROOT, "nebula", "objects", "speakeasy_whitespace_50000_torch.model")
+    with open(os.path.join(REPOSITORY_ROOT, "nebula", "objects", "speakeasy_whitespace_50000_vocab.json")) as f:
+        vocab = json.load(f)
+    model_config = {
+        "vocab_size": len(vocab),
+        "maxlen": 512,
+        "chunk_size": 64,  # input splitting to chunks
+        "dModel": 64,  # embedding & transformer dimension
+        "nHeads": 8,  # number of heads in nn.MultiheadAttention
+        "dHidden": 256,  # dimension of the feedforward network model in nn.TransformerEncoder
+        "nLayers": 2,  # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+        "numClasses": 1,  # binary classification
+        "hiddenNeurons": [64],  # classifier ffnn dims
+        "layerNorm": False,
+        "dropout": 0.3,
+        "mean_over_sequence": False,
+        "norm_first": True,
+        "skip_embedding": skip_embedding
+    }
+    state_dict = torch.load(pretrained_model, map_location='cpu')
+    llm = TransformerEncoderChunksOptionalEmbedding(**model_config)
+    llm.load_state_dict(state_dict)
+    llm.eval()
+    return llm
+
+
+def analyse_folder(folder: pathlib.Path, llm_model, embed_baseline, name):
+    for i, report in enumerate(folder.glob("*.json")):
+        print(report.name)
+        x_embed = embed(llm_model, str(report))
+        compute_score(llm_model, x_embed)
+        explainer = shap.GradientExplainer(model_no_embed, data=embed_baseline)
+        explanations = explainer.shap_values(x_embed, nsamples=50)
+        plot_shap_values(explanations, f"{name}_{i}")
+
+
+model = load_model()
+model_no_embed = load_model(skip_embedding=True)
+
+malware_folder = pathlib.Path(os.path.join(REPOSITORY_ROOT, "evaluation", "explanation", "malware"))
+goodware_folder = pathlib.Path(os.path.join(REPOSITORY_ROOT, "evaluation", "explanation", "goodware"))
+
+baseline_report = os.path.join(REPOSITORY_ROOT, r"emulation", "report_baseline.json")
+x_baseline = tokenize_sample(baseline_report)
+x_embed_baseline = embed(model, baseline_report)
+
+# print(f"\n[!!!] MALWARE FOLDER")
+# analyse_folder(malware_folder, model_no_embed, x_embed_baseline)
+
+print(f"\n[!!!] GOODWARE FOLDER")
+analyse_folder(goodware_folder, model_no_embed, x_embed_baseline, "goodware")
