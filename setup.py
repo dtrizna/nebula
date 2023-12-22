da449cfb7c5d897cf3dbb6991e236c0064041560
Dmitrijs Trizna <ditrizna@protonmail.com>
2023-12-22 16:06:23 +0100

    pip object config

--- setup.py
+++ setup.py
@@ -12,10 +12,21 @@ with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="UTF-8"
 with open("requirements.txt", encoding="utf-8") as f:
     requirements = [line.strip() for line in f.readlines()]
 
+object_files = {}
+for root, dirs, files in os.walk(os.path.join("nebula", "objects")):
+    for file in files:
+        object_files[os.path.join(root, file)] = os.path.join(root, file)
+
 version = "0.0.4"
-package_data = {
-    'nebula': 'objects/*.*'
-}
+package_data = object_files
+# package_data = {
+#     'speakeasy_config': 'objects/speakeasy_config.json',
+#     'bpe_vocab': 'objects/bpe_50000_vocab.json',
+#     'bpe_sentencepiece_model': 'objects/bpe_50000_sentencepiece.model',
+#     'bpe_torch_model': 'objects/bpe_50000_torch.model',
+#     'whitespace_vocab': 'objects/whitespace_50000_vocab.json',
+#     'whitespace_torch_model': 'objects/whitespace_50000_torch.model',
+# }
 setup(
     name="nebula",
     author="Dmitrijs Trizna",
