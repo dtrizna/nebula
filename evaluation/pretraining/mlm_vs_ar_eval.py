4982c5082658a9b2c5247f81e834006fc872dce9
Dmitrijs Trizna <ditrizna@protonmail.com>
2024-03-13 16:41:32 +0000

    autoregressive and mlm pretraining results

--- evaluation/pretraining/mlm_vs_ar_eval.py
+++ evaluation/pretraining/mlm_vs_ar_eval.py
@@ -4,7 +4,7 @@ import json
 import numpy as np
 from time import time
 from sklearn.utils import shuffle
-from lightning.lite.utilities.seed import seed_everything
+from lightning.pytorch import seed_everything
 
 # correct path to repository root
 if os.path.basename(os.getcwd()) == "pretraining":
